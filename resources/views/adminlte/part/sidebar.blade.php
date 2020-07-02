0562bd379eb643f126a59fb29ede4604b1a15018
Muhammad Fahrie Fatihah <48182571+fahriefatihah@users.noreply.github.com>
2020-07-02 14:03:41 +0700

    Add files via upload

--- resources/views/adminlte/part/sidebar.blade.php
+++ resources/views/adminlte/part/sidebar.blade.php
@@ -1,11 +1,11 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <img src="{{asset ('/adminlte/dist/img/AdminLTELogo.png')}}"
+      <img src="{{asset ('/adminlte/dist/img/bg-header-yt2.png')}}"
            alt="AdminLTE Logo"
            class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">M2F DATA</span>
     </a>
 
     <!-- Sidebar -->
@@ -13,10 +13,10 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset ('/adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset ('/adminlte/dist/img/keren.jpeg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Fahrie Fatihah</a>
         </div>
       </div>
 
